4dec1b592e083d1b5ba87a2c750ef8ff37aaeca7
Salvador <frrslo@yahoo.com>
2023-01-10 14:54:39 -0500

    added product.js under models and wrote basic code

---
+++ server/db/models/product.js
@@ -0,0 +1,41 @@
+const require sequelize = require('sequelize')
+const db = require('../db')
+
+
+const Product = db.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: ''
+    }
+})
+
+
+module.exports = Product
