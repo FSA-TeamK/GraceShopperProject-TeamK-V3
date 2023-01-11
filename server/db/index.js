8a5a7eaf57b9a16711e2453575266fa279f7c89b
Eva Li <evali3101@gmail.com>
2023-01-11 11:25:21 -0500

    edited db cart and assoc

--- server/db/index.js
+++ server/db/index.js
@@ -6,14 +6,20 @@ const User = require('./models/User');
 
 const Product = require('./models/product');
 
+const Cart = require('./models/cart');
+
 //associations could go here!
-User.hasMany(Product);
-Product.belongsTo(User);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.hasMany(Product);
+Product.belongsTo(Cart);
 
 module.exports = {
   db,
   models: {
     User,
-    Product
+    Product,
+    Cart
   },
 }
