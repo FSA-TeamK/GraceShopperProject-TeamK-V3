985fe30786df46dcd7170aeafcc8776afe15aac8
Tien La <tiencla@outlook.com>
2023-01-18 22:16:29 -0500

    add products

--- client/features/Auth/AuthForm.js
+++ client/features/Auth/AuthForm.js
@@ -4,7 +4,6 @@ import { authenticate } from '../../app/store';
 import axios from 'axios';
 import { useState } from 'react';
 
-
 /**
   The AuthForm component can be used for Login or Sign Up.
   Props for Login: name="login", displayName="Login"
@@ -18,97 +17,97 @@ const AuthForm = ({ name, displayName }) => {
   const [users, setUsers] = useState([]);
   const [showInfo, setShowInfo] = useState(false);
   const [info, setInfo] = useState([]);
-}
 
   const user = useSelector((state) => state.auth.me);
   
-
   const getUsers = async () => {
     const { data } = await axios.get('/api/users');
     setUsers(data);
     setShowUsers(true);
   };
-
-  // const getInfo = async () => {
-  //   const { data } = await axios.get('/api/userId');  
-  //   setUsers(data);
-  //   setShowInfo(true);
-  // };
-
-
+  
+  const getInfo = async () => {
+    const { data } = await axios.get('/api/userId');
+    setUsers(data);
+    setShowInfo(true);
+  };
+  
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const formName = evt.target.name;
     const username = evt.target.username.value;
     const password = evt.target.password.value;
-    console.log(evt.target.username.value)
+    console.log(evt.target.username.value);
     dispatch(authenticate({ username, password, method: formName }));
   };
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="username">
+            <small>Username</small>
+          </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+      </form>
+      {user && user.isAdmin ? (
+        <button onClick={getUsers}>User Info</button>
+      ) : null}
+      {showUsers ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td>{user.email}</td>
+                  <td>{user.username}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : null}
+      {user && !user.isAdmin ? (
+        <button onClick={getInfo}>Update Info</button>
+      ) : null}
+      {showInfo ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            {info.map((user) => (
+              <tr key={user.id}>
+                <td>{user.email}</td>
+                <td>{user.username}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
+      ;
+    </div>
+  )
+};
 
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit} name={name}>
-//         <div>
-//           <label htmlFor="username">
-//             <small>Username</small>
-//           </label>
-//           <input name="username" type="text" />
-//         </div>
-//         <div>
-//           <label htmlFor="password">
-//             <small>Password</small>
-//           </label>
-//           <input name="password" type="password" />
-//         </div>
-//         <div>
-//           <button type="submit">{displayName}</button>
-//         </div>
-//       </form>
 
-
-//     {user && user.isAdmin ? <button onClick={getUsers}>User Info</button>: null}
-//     {showUsers ? <table>
-//       <thead>
-//         <tr>
-//           <th>Email</th>
-//           <th>Username</th>
-
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {users.map((user) => {
-
-//           return (
-//             <tr key={user.id}>
-//               <td>{user.email}</td>
-//               <td>{user.username}</td>
-//             </tr>
-//           ) 
-//         })}
-//       </tbody>
-//     </table> : null}
-//     </div>
-//   );
-       
-//   // {user && !user.isAdmin ? <button onClick={getInfo}>Update Info</button> : null}
-//   // {showInfo ? (
-//   //   <table>
-//   //       <thead>
-//   //           <tr>
-//   //               <th>Email</th>
-//   //               <th>Username</th>
-//   //           </tr>
-//   //       </thead>
-//   //       <tbody>
-//   //       {info.map((user) => (
-//   //              <tr key={user.id}>
-//   //              <td>{user.email}</td>
-//   //              <td>{user.username}</td>
-//   //            </tr> 
-//   //           ))}
-//   //       </tbody>
-//   //   </table>)
-//   //    : null};
- 
-
-export default AuthForm
+export default AuthForm;
